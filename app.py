b55c1fcaf3eac2701bd2bb777e30943472654ed5
Antonio <toniosilva615@gmail.com>
2026-05-20 01:45:20 +0000

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import pandas as pd
+import streamlit as st
+url = "https://script.google.com/home/projects/1qIyzAZnN7cH1_Kgnpli00-GeGP9ASl52TagViYIgM2psJUHbcNREFf7a/edit"
+# 1. Carrega os dados brutos
+dados_brutos = pd.read_json(url)
+# 2. Organiza colunas e linhas (Pega a 1ª linha como cabeçalho)
+df = pd.DataFrame(dados_brutos.values[1:], columns=dados_brutos.values[0])
+# 3. Formatação de Tipos (Datetime e Float)
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df['temperatura'] = df['temperatura'].astype(float)
+df['umidade'] = df['umidade'].astype(float)
+# 4. Interface Streamlit
+st.title("Monitoramento IoT")
+# Gráfico de linhas usando o timestamp como eixo X
+st.subheader("Evolução Temporal")
+st.line_chart(df.set_index('timestamp')[['temperatura', 'umidade']])
+# Tabela de dados
+st.subheader("Dados Brutos")
+st.dataframe(df)
